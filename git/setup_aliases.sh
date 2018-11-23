2591bf4fce5f741f31c854386a9dfd35d556cb4c
Thomas Maier <tmaier@cern.ch>
2018-11-23 15:12:52 +0100

    add git alias

--- git/setup_aliases.sh
+++ git/setup_aliases.sh
@@ -8,3 +8,4 @@ git config --global alias.l "log"
 git config --global alias.ll "log --pretty=oneline"
 git config --global alias.d diff
 git config --global alias.dn "diff --name-status"
+git config --global alias.dw "diff -w"
